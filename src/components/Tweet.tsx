28275840a041ad6bb179c2a3234b9ac96756d092
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-13 15:17:34 -0400

    styded css on sidebar, instaled phosphor icon

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,10 +1,16 @@
-export function Tweet(props){
+interface TweetProps{
+  user: string
+  children: string
+  likes?: number
+}
+
+export function Tweet(props: TweetProps){
   console.log(props)
   return(
-    <div>
+    <div className="tweet" style={{borderRadius: 8, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
       <strong>{props.user}</strong>
-      <p>{props.content}</p>
-      <button>Like</button>
+      <p>{props.children}</p>
+      <button>Like {props.likes ?? 0}</button>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,63 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Tweet } from './components/Tweet'
+import { Hash, House, Bell, FileText, BookmarkSimple, User, DotsThreeCircle, Envelope, Sparkle } from 'phosphor-react'
+
+import './global.css';
+import twitterLogo from './assets/logo.tweet.svg'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    
-    <div>
-      <Tweet user="Diego" content="meu primeiro tweet"/>
-      <Tweet user="Jessica" content="hello world"/>
-      <Tweet user="Leticia" content="testandooo"/>
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img className='logo' src={twitterLogo} alt="Logo" />
+        <nav className='main-navigation'>
+          <a className='active' href="">
+            <House weight='fill'/>
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className='new-tweet' type='button'>
+          Tweet
+        </button>
+      </aside>
+
+      <div className="content">
+          <main className='timeline'>
+            <div className='timeline-header'>
+              Home
+              <Sparkle />
+            </div>
+          </main>
+      </div>
     </div>
-  
   </React.StrictMode>,
 )
