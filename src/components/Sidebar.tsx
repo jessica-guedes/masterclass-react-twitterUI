e53e3b63f0ffda9077c7a73278a0b8bc5524c46b
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-03-20 11:32:27 -0400

    layout responsivo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Hash, House, Bell, FileText, BookmarkSimple, User, DotsThreeCircle, Envelope } from 'phosphor-react'
+import { Hash, House, Bell, FileText, BookmarkSimple, User, DotsThreeCircle, Envelope, Pencil } from 'phosphor-react'
 import { NavLink } from 'react-router-dom'
 import twitterLogo from '../assets/logo.tweet.svg'
 
@@ -11,40 +11,41 @@ export function Sidebar(){
         <nav className='main-navigation'>
           <NavLink to="/">
             <House weight='fill'/>
-            Home
+            <span>Home</span>
           </NavLink>
           <a href="">
             <Hash />
-            Explore
+            <span>Explore</span>
           </a>
           <a href="">
             <Bell />
-            Notifications
+            <span>Notifications</span>
           </a>
           <a href="">
             <Envelope />
-            Messages
+            <span>Messages</span>
           </a>
           <a href="">
             <BookmarkSimple />
-            Bookmarks
+            <span>Bookmarks</span>
           </a>
           <a href="">
             <FileText />
-            Lists
+            <span>Lists</span>
           </a>
           <a href="">
             <User />
-            Profile
+            <span>Profile</span>
           </a>
           <a href="">
             <DotsThreeCircle />
-            More
+            <span>More</span>
           </a>
         </nav>
 
         <button className='new-tweet' type='button'>
-          Tweet
+          <Pencil />
+          <span>Tweet</span>
         </button>
       </aside>
   )
